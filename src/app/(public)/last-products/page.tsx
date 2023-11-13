42efd83e80d4f224d827f3af61b599e6da12e333
mac1 <mac1@MacBookPro-2.local>
2023-11-13 17:56:34 +0500

    update responsive

--- src/app/(public)/last-products/page.tsx
+++ src/app/(public)/last-products/page.tsx
@@ -60,60 +60,68 @@ const LastProducts = () => {
     ],
   };
   return (
-    <Slider {...settings}>
-      <div className="hero__aside" data-aos="zoom-in">
-        <h1>
-          Bizning <br /> So'ngi Mahsulotlarimiz
-        </h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
-          praesentium neque, soluta quis quisquam aperiam eius officia numquam
-          commodi hic molestiae quod ad officiis, veniam, eveniet repellendus.
-        </p>
-        <Link href="/allproducts">
-          <button>Barchasi 👉</button>
-        </Link>
+    <>
+      <div className="product__title">
+        <h2>So'ngi mahsulotlarimiz</h2>
       </div>
-      {loading ? (
-        <div>
-          <h2>Loading....</h2>
+      <Slider {...settings}>
+        <div className="hero__aside" data-aos="zoom-in">
+          <h1>
+            Bizning <br /> So'ngi Mahsulotlarimiz
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
+            praesentium neque, soluta quis quisquam aperiam eius officia numquam
+            commodi hic molestiae quod ad officiis, veniam, eveniet repellendus.
+          </p>
+          <Link href="/allproducts">
+            <button>Barchasi 👉</button>
+          </Link>
         </div>
-      ) : (
-        latestProducts?.map((product) => (
-          <div className="last_products" key={product?._id}>
-            <div className="last__img">
-              <Image
-                src={product?.image.url}
-                alt={product?.title || "Product"}
-                fill
-                objectFit="cover"
-              />
-            </div>
-            <Link href={`/category/${product?._id}`} className="last__content">
-              <h3>{product?.title}</h3>
-              <p>Miqdori: {product?.quantity}</p>
-              <p>Narxi: {product?.price} sum</p>
-            </Link>
-            <div className="button__wrapper">
-              <button
-                onClick={() =>
-                  addToCart(
-                    product?._id,
-                    product?.image.url,
-                    product?.title,
-                    product?.description,
-                    product?.price
-                  )
-                }
-                className="product__btn">
-                cartga qo'shish
-              </button>
-            </div>
+
+        {loading ? (
+          <div>
+            <h2>Loading....</h2>
           </div>
-        ))
-      )}
-      {/* </div> */}
-    </Slider>
+        ) : (
+          latestProducts?.map((product) => (
+            <div className="last_products" key={product?._id}>
+              <div className="last__img">
+                <Image
+                  src={product?.image.url}
+                  alt={product?.title || "Product"}
+                  fill
+                  objectFit="cover"
+                />
+              </div>
+              <Link
+                href={`/category/${product?._id}`}
+                className="last__content">
+                <h3>{product?.title}</h3>
+                <p>Miqdori: {product?.quantity}</p>
+                <p>Narxi: {product?.price} sum</p>
+              </Link>
+              <div className="button__wrapper">
+                <button
+                  onClick={() =>
+                    addToCart(
+                      product?._id,
+                      product?.image.url,
+                      product?.title,
+                      product?.description,
+                      product?.price
+                    )
+                  }
+                  className="product__btn">
+                  cartga qo'shish
+                </button>
+              </div>
+            </div>
+          ))
+        )}
+        {/* </div> */}
+      </Slider>
+    </>
   );
 };
 
--- src/components/layout/front/header/page.tsx
+++ src/components/layout/front/header/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Cookies } from "js-cookie";
+import Cookies from "js-cookie";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
